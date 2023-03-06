976daf27aaf781a0efab0a83bcd48134beb9c07b
ROMAN <poman207575@gmail.com>
2023-03-06 20:13:14 +0200

    dev

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-// $(".option").click(function(){
-//     $(".option").removeClass("active");
-//     $(this).addClass("active");
-    
-//  });
-
 document.querySelectorAll('.option').forEach((element, index, arr) => {
-    element.addEventListener('click', (event) => {
-        document.querySelector('.option.active').classList.remove('active');
-        console.log(event.target.classList.add('active'));
+    element.addEventListener('click', () => {
+        let elem = event.target.closest('.option'); 
+        if(elem.classList[1] != 'active'){
+            document.querySelector('.option.active').classList.remove('active');
+            elem.classList.add('active');
+        }
     });
 });
